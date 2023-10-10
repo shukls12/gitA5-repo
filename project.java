d3cc959ec237ce6710f856cb83feff422471a228
root <root@ip-172-31-19-238.eu-north-1.compute.internal>
2023-10-10 07:54:29 +0000

    in featureAbranch added one more line

--- project.java
+++ project.java
@@ -24,3 +24,7 @@ void  StringToIntExample {
         int number = Integer.parseInt(numberAsString);
         System.out.println(number);
     }
+//Adding one more block in featureA
+System.out.println("In FeatureB");
+
+}
